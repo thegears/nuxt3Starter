147a1a226554c07e26f47739a07947ebe79c7980
thegears <semh.1362@gmail.com>
2022-06-17 23:44:12 +0300

    Update primevue.ts

--- plugins/primevue.ts
+++ plugins/primevue.ts
@@ -2,5 +2,7 @@ import { defineNuxtPlugin } from "#app";
 import PrimeVue from "primevue/config";
 
 export default defineNuxtPlugin((nuxtApp) => {
-    nuxtApp.vueApp.use(PrimeVue, {ripple: true});
+    nuxtApp.vueApp.use(PrimeVue, {
+        ripple: true
+    });
 });
