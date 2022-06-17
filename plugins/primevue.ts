10c69294b9d2fb7846ee0bf79fb8a4d470a35486
thegears <semh.1362@gmail.com>
2022-06-17 21:30:14 +0300

    Update primevue.ts

--- plugins/primevue.ts
+++ plugins/primevue.ts
@@ -1,6 +1,5 @@
 import { defineNuxtPlugin } from "#app";
 import PrimeVue from "primevue/config";
-import Button from "primevue/button";
 
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.use(PrimeVue, {ripple: true});
