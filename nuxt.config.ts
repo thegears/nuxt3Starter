31f04b3449f2407a614b01783265920627336604
thegears <semh.1362@gmail.com>
2022-06-17 21:35:39 +0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import { defineNuxtConfig } from 'nuxt';
 
 export default defineNuxtConfig({
     buildModules: [
-        'nuxt-windicss','@nuxtjs/pwa'
+        '@nuxtjs/pwa'
     ],
     serverMiddleware: [
         { path: "/api", handler: "~/server/api.ts" },
