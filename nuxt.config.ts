3958d6207e09f10adb9728e98e9ca223100f2c79
thegears <semh.1362@gmail.com>
2022-06-17 21:19:51 +0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,14 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from 'nuxt';
 
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     buildModules: [
         'nuxt-windicss','@nuxtjs/pwa'
     ],
     serverMiddleware: [
         { path: "/api", handler: "~/server/api.ts" },
+    ],
+    css: [
+        'primevue/resources/themes/saga-blue/theme.css',
+        'primevue/resources/primevue.css'
     ]
-})
+});
